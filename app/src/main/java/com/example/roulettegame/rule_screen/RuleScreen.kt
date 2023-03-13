e9fbd695198787b13dc2cefcdc86bd92c8d38794
jakobakoba <jakobakoba@gmail.com>
2023-03-13 14:17:32 +0600

    added UI structure

--- app/src/main/java/com/example/roulettegame/rule_screen/RuleScreen.kt
+++ app/src/main/java/com/example/roulettegame/rule_screen/RuleScreen.kt
@@ -1,14 +1,17 @@
 package com.example.roulettegame.rule_screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.roulettegame.R
 
 
 @Composable
@@ -20,10 +23,50 @@ fun RuleScreen() {
         Text(
             text = "0",
             fontWeight = FontWeight.Bold,
-            fontSize = 35.sp,
-            color = Color.White
+            fontSize = 60.sp,
+            color = Color.White,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .wrapContentHeight()
+                .wrapContentWidth()
         )
+        Box(
+            modifier = Modifier
+                .weight(1F)
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.roulette),
+                contentDescription = "Roulette",
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+            Image(
+                painter = painterResource(id = R.drawable.strelka),
+                contentDescription = "Pointer",
+                modifier = Modifier
+                    .fillMaxSize()
+            )
 
+        }
+        androidx.compose.material.Button(
+            onClick = { },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp)
+                .padding(20.dp)
+
+
+        ) {
+            Text(
+                text = "Start",
+                color = Color.White,
+                fontSize = 30.sp
+
+            )
+        }
 
     }
 
