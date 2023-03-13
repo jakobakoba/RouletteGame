0bc6f4deebc6a467a2fecc6b8a38519afdd8e53a
jakobakoba <jakobakoba@gmail.com>
2023-03-13 17:22:28 +0600

    added rotation functionality, NumberUtil object

--- app/src/main/java/com/example/roulettegame/rule_screen/RuleScreen.kt
+++ app/src/main/java/com/example/roulettegame/rule_screen/RuleScreen.kt
@@ -1,27 +1,53 @@
 package com.example.roulettegame.rule_screen
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.roulettegame.R
+import com.example.roulettegame.utils.NumberUtil
+import kotlin.math.roundToInt
 
 
 @Composable
 fun RuleScreen() {
+
+    var rotationValue by remember {
+        mutableStateOf(0f)
+    }
+
+    var number by remember {
+        mutableStateOf(0)
+    }
+    val angle: Float by animateFloatAsState(
+        targetValue = rotationValue,
+        animationSpec = tween(
+            durationMillis = 4000
+        ),
+        finishedListener = {
+            val index = (360f - (it % 360)) / (360f / NumberUtil.list.size)
+            number = NumberUtil.list[index.roundToInt()]
+
+        }
+
+    )
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
-            text = "0",
+            text = number.toString(),
             fontWeight = FontWeight.Bold,
             fontSize = 60.sp,
             color = Color.White,
@@ -41,6 +67,7 @@ fun RuleScreen() {
                 contentDescription = "Roulette",
                 modifier = Modifier
                     .fillMaxSize()
+                    .rotate(angle)
             )
             Image(
                 painter = painterResource(id = R.drawable.strelka),
@@ -51,7 +78,9 @@ fun RuleScreen() {
 
         }
         androidx.compose.material.Button(
-            onClick = { },
+            onClick = {
+                rotationValue = (720..1080).random().toFloat() + angle
+            },
             colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
             modifier = Modifier
                 .fillMaxWidth()
